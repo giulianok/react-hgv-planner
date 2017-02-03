0415b597dc9eae1b5acd3e845874a4a41340560d
Giuliano Kranevitter <info@giulianok.com>
2017-02-03 18:45:35 -0500

    First commit: the structure

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+
+    render() {
+        return (
+            <h1>Testing!</h1>
+        )
+    }
+
+}
+
+ReactDOM.render(<Layout/>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    context: path.join(__dirname, "src"),
+    entry: "./js/app.js",
+    output: {
+        path: __dirname + "/src/",
+        filename: "app.min.js"
+    },
+    devtool: "inline-sourcemap",
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0'],
+                    plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
+    ]
+};
