ec8f6385441d69efb172cb1ccf44abd84032b237
GIULIANO-PC\giuliano <info@giulianok.com>
2017-02-05 18:39:55 -0500

    Planner ready and working.

--- src/js/components/GMap.js
+++ src/js/components/GMap.js
@@ -1,12 +1,37 @@
 import React from "react";
 import GoogleMapsLoader from "google-maps";
 
+// const mock = {
+//     origin: 'Chicago, IL',
+//     destination: 'Los Angeles, CA',
+//     waypoints: [
+//         {
+//             location: 'Joplin, MO',
+//             stopover: false
+//         },{
+//             location: 'Oklahoma City, OK',
+//             stopover: true
+//         }],
+//     provideRouteAlternatives: false,
+//     travelMode: 'DRIVING',
+//     drivingOptions: {
+//         departureTime: new Date(/* now, or future date */),
+//         trafficModel: 'pessimistic'
+//     },
+//     unitSystem: google.maps.UnitSystem.IMPERIAL
+// }
 
 export default class GMap extends React.Component {
 
     componentDidMount() {
-        const mapElement = document.getElementById('map');
+        GoogleMapsLoader.load((google) => {
+            this.renderMap(google);
+            this.setupServices();
+        });
+    }
 
+    renderMap(google) {
+        const mapElement = this.getMapElement();
         const options = {
             center: {
                 lat: 55.378051,
@@ -15,28 +40,80 @@ export default class GMap extends React.Component {
             zoom: 6
         };
 
-        GoogleMapsLoader.load((google) => {
-            new google.maps.Map(mapElement, options);
+        this.setState({
+            map: new google.maps.Map(mapElement, options)
         });
     }
 
-    geolocate() {
-
+    setupServices() {
+        this.setState({
+            directionsService: new google.maps.DirectionsService(),
+            directionsDisplay: new google.maps.DirectionsRenderer()
+        });
     }
 
+    getMapElement() {
+        return document.getElementById('map');
+    }
 
+    renderRoutes(origin, destinations) {
+        const {map, directionsDisplay} = this.state;
+        const request = this.buildRequestObject(origin, destinations);
 
-    fillInAddress() {
+        this.clearRoutes();
+        this.setupMap();
+        this.displayRoutes(request);
+    }
 
+    clearRoutes() {
+        const {directionsDisplay} = this.state;
+        directionsDisplay.setMap(null);
+    }
+
+    setupMap() {
+        const {directionsDisplay, map} = this.state;
+        directionsDisplay.setMap(map);
+    }
+
+    buildRequestObject(origin, destinations) {
+        return {
+            origin: origin,
+            destination: this.getDestination(destinations),
+            waypoints: this.getWaypoints(destinations),
+            travelMode: 'DRIVING'
+        };
+    }
+
+    displayRoutes(request) {
+        const {directionsDisplay, directionsService} = this.state;
+        directionsService.route(request, function(result, status) {
+            console.log('status', status);
+            if (status == 'OK') {
+                directionsDisplay.setDirections(result);
+            }
+        });
+    }
+
+    getDestination(destinations) { return destinations[destinations.length - 1]; }
+
+    getWaypoints(destinations) {
+        if (destinations.length <= 1)
+            return null;
+
+        destinations.pop();
+
+        return destinations.map((destination) => {
+            return {
+                location: destination,
+                stopover: true
+            }
+        })
     }
 
     render() {
         return (
             <div>
                 <div id="map"></div>
-                <div id="locationField">
-                    <input id="autocomplete" placeholder="Enter your address" onFocus={this.geolocate.bind(this)} type="text"></input>
-                </div>
             </div>
         )
     }
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,23 +1,72 @@
 import React from "react";
 import { connect } from "react-redux"
-import Form from "./Form";
 import GMap from "./GMap";
+import Field from "./Field";
+import MultipleDestinations from "./MultipleDestinations";
 
 
 @connect((store) => {
     return {
-        places: store.place
+        place: store.place
     }
 })
 export default class Layout extends React.Component {
 
     // API KEY: AIzaSyBAFWLD0zvRsV6caGfjMsCoaX3_V-Go-4I
 
+    componentDidMount() {
+    }
+
+    getInformation() {
+        console.log('getting info...');
+        const {origin, destinations} = this.props.place;
+
+        if (!this.isOriginValid(origin) || !this.areDestinationsValid(destinations)) {
+            alert(`Please complete the Origin and Destination(s) with real UK location`);
+            return;
+        }
+
+        const originConverted = this.convertPlaceToLatLng(origin);
+        const destinationsConverted = destinations.map(d => this.convertPlaceToLatLng(d));
+
+        this.refs.map.renderRoutes(originConverted, destinationsConverted);
+    }
+
+    isOriginValid(origin) { return this.isValidPlace(origin); }
+
+    areDestinationsValid(destinations) {
+        for (const index in destinations) {
+            if (!destinations[index]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    isValidPlace(place) {
+        if (!place) {
+            return false;
+        }
+        return true;
+    }
+
+    convertPlaceToLatLng(place) { return new google.maps.LatLng(place.geometry.location.lat(), place.geometry.location.lng()); }
+
     render() {
         return (
             <div>
-                <Form />
-                <GMap />
+                <form action="#">
+                    <fieldset>
+                        <legend>Journeys Details</legend>
+
+                        <Field name="origin" label="Origin" />
+
+                        <MultipleDestinations limit="4" />
+
+                        <button type="button" onClick={this.getInformation.bind(this)}>Plan Journey</button>
+                    </fieldset>
+                </form>
+                <GMap ref="map" />
             </div>
         )
     }
